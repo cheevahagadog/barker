58d1b15b22a4e641d2f35b86f9e6bcaab1f6f3e1
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-24 20:42:38 -0600

    organizing config layout

--- config.py
+++ config.py
@@ -5,39 +5,8 @@ import time
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'barker.db')
 
-# Bookmark configs
-EXCLUDE_FOLDERS = ['Personal', 'Projects', 'Mealio', 'FrontAnalytics', "Dependency hell!", "PC Automation w/ R"]
-TOP_FOCUS_FOLDERS = ['Data Science', 'Review']
-SUB_FOCUS_FOLDERS = ['Great Blogs']
-NUM_LINKS_TO_INCLUDE = 7
-URL_STEMS_TO_EXCLUDE = ['http://stackoverflow.com', "https://docs.google"]
-SEARCH_RECENT = False
-
-# Meetup settings
-MEETUP_API_KEY = os.environ.get("MEETUP_API_KEY", None)
-DO_NOT_INCLUDE_GROUPS = []
-
-RUN = {
-    "MONTHLY": 15,
-    "WEEKLY": "SAT",
-    "DAILY": "15:00",
-    "HOURLY": '15'
-}
-
-EMAIL = False
-DELIVERY_DIR = os.path.join(
-    os.path.abspath(__file__),
-    'delivery'
-    )
-
-# mail server settings
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME", None)
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", None)
-
-# administrator list
-
+# ---- NEWSLETTER/PERSONAL SETTINGS ------ #
 PERSONAL_DATA = {
     'NAME': "Nathan",
     'TWITTER_HANDLE': "data_cheeves",
@@ -48,4 +17,25 @@ PERSONAL_DATA = {
     'DATE': time.strftime("%x")
 }
 
+# ---- BOOKMARK SETTINGS ------ #
+EXCLUDE_FOLDERS = ['Personal', 'Projects', 'Mealio', 'FrontAnalytics', "Dependency hell!", "PC Automation w/ R"]
+TOP_FOCUS_FOLDERS = ['Data Science', 'Review']
+SUB_FOCUS_FOLDERS = ['Great Blogs']
+NUM_LINKS_TO_INCLUDE = 7
+URL_STEMS_TO_EXCLUDE = ['http://stackoverflow.com', "https://docs.google"]
+SEARCH_RECENT = False
 
+
+# ---- MEETUP SETTINGS ------ #
+MEETUP_API_KEY = os.environ.get("MEETUP_API_KEY", None)
+DO_NOT_INCLUDE_GROUPS = []
+
+
+# ---- EMAIL SETTINGS --------#
+EMAIL = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME", None)
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", None)
+
+
+# ---- STORAGE SETTINGS ------#
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'barker.db')
