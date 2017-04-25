96813db21030d6934985201ee4de709e4ddff4ba
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-24 21:56:34 -0600

    moved lots of code to helper fns

--- barker/create_newsletter.py
+++ barker/create_newsletter.py
@@ -4,28 +4,19 @@
 from jinja2 import Environment, FileSystemLoader
 
 import config
-from barker.helpers import DataBaseInterface, fetch_newsletter_data, get_page_summary, \
-    get_upcoming_meetup_calendar_events
+from barker.helpers import DataBaseInterface, fetch_newsletter_data, creating_link_info, create_meetup_info
+
+dbi = DataBaseInterface()
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template("templates/skeleton.html")
 
 if __name__ == '__main__':
-    dbi = DataBaseInterface()
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template("templates/skeleton.html")
-
-    # Get bookmark data
     df = dbi.load_data_from_table('bookmarks')
-    df = fetch_newsletter_data(df)
-    bookmark_data = df.set_index('name')['url'].to_dict()
-
-    # Summarize pages
-    print("Getting page information...")
-    bookmark_data = {k: [get_page_summary(k, v), v] for k, v in bookmark_data.items()}
-
-    # add in local meetups
-    params = {"state": 'UT', "key": config.MEETUP_API_KEY, "page": 10, "order": 'time'}
-    print("Getting upcoming meetup info...")
-    meetup_data = get_upcoming_meetup_calendar_events(params)
-    html = template.render(bookmark_data=bookmark_data, page_data=config.PERSONAL_DATA, meetup_data=meetup_data)
-    print("newsletter html updated!")
-    with open('templates/newsletter.html', 'w') as myFile:
-        myFile.write(html)
+    success, df = fetch_newsletter_data(df, use_recent=config.FOCUS_RECENT)
+    if success:
+        bookmark_dict = creating_link_info(df)
+        success, meetup_data = create_meetup_info()
+        html = template.render(bookmark_data=bookmark_dict, page_data=config.PERSONAL_DATA, meetup_data=meetup_data)
+        print("newsletter html updated!")
+        with open('templates/newsletter.html', 'w') as myFile:
+            myFile.write(html)
