47d28597c7f8f61dc4cb1dd0791fbaded73a7520
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-26 09:22:55 -0600

    email service is working

--- app/email_service.py
+++ app/email_service.py
@@ -1,18 +1,29 @@
 # -*- coding:utf-8 -*-
 """http://stackoverflow.com/questions/882712/sending-html-email-using-python"""
+
+import smtplib
 import yagmail
 import codecs
-from ..barker import config
+import config
 
 
 if __name__ == "__main__":
-    # yagmail.register("{}".format(config.MAIL_USERNAME), "{}".format(config.MAIL_PASSWORD))
-    yag = yagmail.SMTP("{}".format(config.MAIL_USERNAME))
-    subject = "Your weekly newsletter!"
+    try:
+        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server_ssl.login('nathan.cheever12@gmail.com', 'NYCis8rH0^^e')
+    except Exception as e:
+        print("something went wrong in email: {}".format(e))
+    else:
+        subject = "Your weekly newsletter!"
+        html = codecs.open("templates/newsletter.html", 'r', 'utf-8').read()
+        server_ssl.sendmail(msg=html, from_addr=config.MAIL_USERNAME, to_addrs=config.MAIL_USERNAME)
+        server_ssl.close()
 
-    f = codecs.open("templates/newsletter.html", 'r', 'utf-8')
-    html = f.read()
-    yag.send(to="{}".format(config.MAIL_USERNAME), subject=subject, contents=html)
+    # # yagmail.register("{}".format(config.MAIL_USERNAME), "{}".format(config.MAIL_PASSWORD))
+    # with yagmail.SMTP("{}".format(config.MAIL_USERNAME)) as yag:
+    #
+    #
+    #     yag.send(to="{}".format(config.MAIL_USERNAME), subject=subject, contents=html)
 
     # message = Message(From="{}".format(config.MAIL_USERNAME),
     #                   To="{}".format(config.MAIL_USERNAME))
