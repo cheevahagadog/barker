47f0779ac8ceb74554bf01f4167c62a66253bb63
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-26 09:43:18 -0600

    email with html template working and sending

--- app/email_service.py
+++ app/email_service.py
@@ -2,36 +2,32 @@
 """http://stackoverflow.com/questions/882712/sending-html-email-using-python"""
 
 import smtplib
-import yagmail
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import html2text
 import codecs
 import config
 
 
 if __name__ == "__main__":
+    msg = MIMEMultipart('alternative')
+    with codecs.open("app/templates/newsletter.html", 'r', 'utf-8') as fp:
+        html = fp.read()
+        text = html2text.html2text(html)
     try:
-        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server_ssl.login('nathan.cheever12@gmail.com', 'NYCis8rH0^^e')
+        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        s.login(config.MAIL_USERNAME, config.MAIL_PASSWORD)
     except Exception as e:
-        print("something went wrong in email: {}".format(e))
+        print("something went wrong in setting up smtp: {}".format(e))
     else:
-        subject = "Your weekly newsletter!"
-        html = codecs.open("templates/newsletter.html", 'r', 'utf-8').read()
-        server_ssl.sendmail(msg=html, from_addr=config.MAIL_USERNAME, to_addrs=config.MAIL_USERNAME)
-        server_ssl.close()
+        msg['Subject'] = "Your weekly newsletter!"
+        msg['From'] = config.MAIL_USERNAME
+        msg['To'] = config.MAIL_USERNAME
 
-    # # yagmail.register("{}".format(config.MAIL_USERNAME), "{}".format(config.MAIL_PASSWORD))
-    # with yagmail.SMTP("{}".format(config.MAIL_USERNAME)) as yag:
-    #
-    #
-    #     yag.send(to="{}".format(config.MAIL_USERNAME), subject=subject, contents=html)
-
-    # message = Message(From="{}".format(config.MAIL_USERNAME),
-    #                   To="{}".format(config.MAIL_USERNAME))
-    # message.Subject = "An HTML Email"
-    # message.Html = """<p>Hi!<br>
-    #    How are you?<br>
-    #    Here is the <a href="http://www.python.org">link</a> you wanted.</p>"""
-    #
-    # sender = Mailer('smtp.gmail.com', port=587, use_tls=True, usr=config.MAIL_USERNAME, pwd=config.MAIL_PASSWORD)
-    # print("sending...")
-    # sender.send(message)
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+        msg.attach(part1)
+        msg.attach(part2)
+        s.sendmail(msg=msg.as_string(), from_addr=config.MAIL_USERNAME, to_addrs=config.MAIL_USERNAME)
+        s.quit()
