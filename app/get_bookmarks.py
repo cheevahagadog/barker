b2fc73d9e82ff0fa90625458962e4014b1e4088b
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-25 19:59:52 -0600

    reorganizing

--- app/get_bookmarks.py
+++ app/get_bookmarks.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 """Fetches the bookmarks from Chrome files and saves them into a SQLite db"""
 
-from barker.helpers import DataBaseInterface, get_bookmarks, transform_json_to_df
+from app.helpers import DataBaseInterface, get_bookmarks, transform_json_to_df
 
 
 if __name__ == '__main__':
