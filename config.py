83e8f460164ebc56bc372b2f0f9a4d13cbea73fc
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-30 09:36:03 -0600

    more helpful var names + comment explanations

--- config.py
+++ config.py
@@ -18,13 +18,22 @@ PERSONAL_DATA = {
     'DATE': time.strftime("%x")
 }
 
+
 # ---- BOOKMARK SETTINGS ------ #
 EXCLUDE_FOLDERS = ['Personal', 'Projects', 'Mealio', 'FrontAnalytics', "Dependency hell!", "PC Automation w/ R"]
-FOCUS_RECENT = True
-TOP_FOCUS_FOLDERS = ['Data Science', 'Review']
-SUB_FOCUS_FOLDERS = ['Great Blogs']
-NUM_LINKS_TO_INCLUDE = 7
+USE_MOST_RECENT_BOOKMARKS = True
+
+# If included, barker will only use bmks from these top level folders
+BOOKMARK_BAR_FOCUS_FOLDERS = ['Data Science', 'Review']
+
+# OTHER_FOCUS_FOLDERS - Use when you want to use bmks from a sub folder with a specific name
+OTHER_FOCUS_FOLDERS = ['Great Blogs']
+
+NUM_LINKS_TO_INCLUDE = 7  # This will be the number of links in the newsletter
 URL_STEMS_TO_EXCLUDE = ['http://stackoverflow.com', "https://docs.google"]
+
+# If barker can't detect where your Chrome bmks are saved, enter full path to Bookmarks directory
+BOOKMARK_PATH = None
 
 
 # ---- MEETUP SETTINGS ------ #
