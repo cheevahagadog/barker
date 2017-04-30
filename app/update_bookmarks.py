fc7dace53c19baa8108d9bd23cfd5927756a3e6f
Nathan Cheever <nathan.cheever12@gmail.com>
2017-04-30 09:38:37 -0600

    reflecting change of get_bookmarks return bool

--- app/update_bookmarks.py
+++ app/update_bookmarks.py
@@ -6,7 +6,8 @@ from app.helpers import DataBaseInterface, get_bookmarks, transform_json_to_df
 
 if __name__ == '__main__':
     dbi = DataBaseInterface()
-    bookmarks = get_bookmarks()
-    data = transform_json_to_df(bookmarks)
-    dbi.save_data(data, table_name='bookmarks')
-    print("Bookmarks table updated, {} entries".format(len(data)))
+    success, bookmarks_dict = get_bookmarks()
+    if success:
+        data = transform_json_to_df(bookmarks_dict)
+        dbi.save_data(data, table_name='bookmarks')
+        print("Bookmarks table updated, {} entries".format(len(data)))
